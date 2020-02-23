09ce12e9b8dee30a49bc93478750efc4c5c6fa7f
HuanPeng123 <18428309956@163.com>
2020-02-23 17:10:44 +0800

    create spring boot prject

---
+++ src/main/java/com/demo/ordermachine/OrderMachineApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ordermachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMachineApplication.class, args);
+    }
+
+}
